c18c12f1f481d596757449b6b44c6c3535513f2d
dac114514 <dashuaicheng2@gmail.com>
2026-04-30 21:47:10 +0800

    fixup: remove unused import

--- app/src/main/java/com/java/myapplication/app/MojiangApp.kt
+++ app/src/main/java/com/java/myapplication/app/MojiangApp.kt
@@ -28,7 +28,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
